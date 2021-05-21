b2fd24deb23b7716528da05685475157c993a931
JuansebastianReyes <reyes_garcia1103@hotmail.com>
2021-05-21 01:31:26 -0500

    Edicion en rutas servidor y puesta a punto de docker-compose

--- go-server-mysql/commons/connections.go
+++ go-server-mysql/commons/connections.go
@@ -9,7 +9,15 @@ import (
 )
 
 func GetConnetion() *gorm.DB {
-	db, err := gorm.Open("mysql", "root:@/tickets?parseTime=true")
+
+	//Variables de Entorno para coneccion con Docker
+	// si se usa phpmyadmin o otra tipo de distribucion de mysql cambiar el password y el Database
+	Driver := "mysql"        //Driver para poder conectarnos a la BD
+	Usser := "root"          //Usuarion con el cual nos logeamos a la BD
+	Password := "tickets123" //Contraseña de accesp a la base de datos del usuario
+	Database := "db_tickets" //Nombre de la base de datos donde vamos a alojar los datos
+
+	db, err := gorm.Open(Driver, Usser+":"+Password+"@/"+Database+"?parseTime=true")
 
 	if err != nil {
 		log.Fatal(err)
--- go-server-mysql/routes/tickets_routes.go
+++ go-server-mysql/routes/tickets_routes.go
@@ -10,6 +10,6 @@ func SetTicketsRoutes(router *mux.Router) {
 	subRoute := router.PathPrefix("/tickets/api").Subrouter()
 	subRoute.HandleFunc("/lista", controllers.GetAll).Methods("GET")
 	subRoute.HandleFunc("/guardar", controllers.Save).Methods("POST")
-	subRoute.HandleFunc("/delete/{id}", controllers.Delete).Methods("POST")
-	subRoute.HandleFunc("/find/{id}", controllers.Get).Methods("GET")
+	subRoute.HandleFunc("/eliminar/{id}", controllers.Delete).Methods("POST")
+	subRoute.HandleFunc("/ticket/{id}", controllers.Get).Methods("GET")
 }
